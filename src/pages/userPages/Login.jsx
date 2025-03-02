87f29568dd25ebe9c83d6f2104e37656183fe07e
Christian.Dev <CAquino.dev@gmail.com>
2025-03-02 14:58:47 +0800

    make a simple login template

--- src/pages/userPages/Login.jsx
+++ src/pages/userPages/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`username: ${username} password:${password}`)
+        setUsername("");
+        setPassword("");
+    }
+
   return (
     <div>
-      Login
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        value={username}
+        placeholder='Username'
+        onChange={(e) => {setUsername(e.target.value)}}
+        />
+        <input 
+        type="password" 
+        value={password}
+        placeholder='Password'
+        onChange={(e) => {setPassword(e.target.value)}}
+        />
+        <button type='submit' >Login</button>
+      </form>
     </div>
   )
 }
