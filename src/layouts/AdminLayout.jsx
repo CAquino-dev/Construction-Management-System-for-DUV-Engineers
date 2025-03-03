8c125a6b653c46ed1905cb61df3e6d25f3bd1c13
JasTenC <117107364+JasTenC@users.noreply.github.com>
2025-03-03 17:06:14 +0800

    fix most of the frontend and make the api's in the backend
    * make a simple login template
    
    * create a template for the register page
    
    * react-router-dom
    
    * admin side nav
    
    * make the register route for the backend
    
    * framer-motion
    
    * side nav complete
    
    * added a footer
    
    * footer compo
    
    * duv logo
    
    * fix routes and added admin dashboar
    
    * medjo responsive na nav
    
    * admin login na may design na
    
    * userLogin na may design na
    
    * home page gui
    
    ---------
    
    Co-authored-by: Christian.Dev <CAquino.dev@gmail.com>

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import AdminNavbar from '../components/adminComponents/AdminNavbar'
 
 const AdminLayout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <AdminNavbar />
+      <Outlet />
+    </>
   )
 }
 
--- src/pages/userPages/Login.jsx
+++ src/pages/userPages/Login.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`username: ${username} password:${password}`)
+        setUsername("");
+        setPassword("");
+    }
+
   return (
-    <div>
-      Login
+    <div className='pt-16 flex items-center justify-center min-h-screen bg-[#f6f6f6]'>
+      <div className='bg-white rounded-2xl shadow-2lg p-8 w-full max-w-md'>
+        <h2 className='text-2xl font-bold text-center mb-6 text-[#4c735c]'>Login</h2>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <input 
+          type="text" 
+          value={username}
+          placeholder='Username'
+          onChange={(e) => {setUsername(e.target.value)}}
+          className='p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4c735c]'
+          />
+          <input 
+          type="password" 
+          value={password}
+          placeholder='Password'
+          onChange={(e) => {setPassword(e.target.value)}}
+          className='p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4c735c]'
+          />
+          <button type='submit' className='bg-[#4c735c] text-white py-3 rounded-lg hover:bg-[#4c735c]/90 cursor-pointer' >Login</button>
+        </form>
+      </div>     
     </div>
   )
 }
