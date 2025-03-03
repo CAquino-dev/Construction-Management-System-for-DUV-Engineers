391536f4226f4a733a1844462c6c40af2e74a6f6
JasTenC <gorduizjustincarl@gmail.com>
2025-03-03 13:47:08 +0800

    fix routes and added admin dashboar

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import Homepage from './pages/userPages/Homepage'
 import Login from './pages/userPages/Login'
 import Register from './pages/userPages/Register'
+import AdminLayout from './layouts/AdminLayout'
+import UserLayout from './layouts/UserLayout'
+import { AdminDashboard } from './pages/admin/AdminDashboard'
+import AdminLogin from './pages/admin/AdminLogin'
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Homepage/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/register' element={<Register/>}></Route>
+          {/* User Routes */} 
+          <Route path='/' element={<UserLayout/>}>
+            <Route index element={<Homepage/>}></Route>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/register' element={<Register/>}></Route>
+          </Route>
+
+          {/* Admin Routes */}
+          <Route path='/admin' element={<AdminLogin/>}></Route>
+            <Route path='/admin-dashboard' element={<AdminLayout/>} >
+            <Route index element={<AdminDashboard/>} />
+          </Route>
         </Routes>
       </Router>
-
-
     </div>
   )
 }
