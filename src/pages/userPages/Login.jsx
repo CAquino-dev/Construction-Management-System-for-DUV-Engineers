73598ca926889b95ed40f76a0de787772366c65b
JasTenC <gorduizjustincarl@gmail.com>
2025-03-03 13:49:51 +0800

    userLogin na may design na

--- src/pages/userPages/Login.jsx
+++ src/pages/userPages/Login.jsx
@@ -13,23 +13,27 @@ const Login = () => {
     }
 
   return (
-    <div>
-        <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input 
-        type="text" 
-        value={username}
-        placeholder='Username'
-        onChange={(e) => {setUsername(e.target.value)}}
-        />
-        <input 
-        type="password" 
-        value={password}
-        placeholder='Password'
-        onChange={(e) => {setPassword(e.target.value)}}
-        />
-        <button type='submit' >Login</button>
-      </form>
+    <div className='pt-16 flex items-center justify-center min-h-screen bg-[#f6f6f6]'>
+      <div className='bg-white rounded-2xl shadow-2lg p-8 w-full max-w-md'>
+        <h2 className='text-2xl font-bold text-center mb-6 text-[#4c735c]'>Login</h2>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <input 
+          type="text" 
+          value={username}
+          placeholder='Username'
+          onChange={(e) => {setUsername(e.target.value)}}
+          className='p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4c735c]'
+          />
+          <input 
+          type="password" 
+          value={password}
+          placeholder='Password'
+          onChange={(e) => {setPassword(e.target.value)}}
+          className='p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4c735c]'
+          />
+          <button type='submit' className='bg-[#4c735c] text-white py-3 rounded-lg hover:bg-[#4c735c]/90 cursor-pointer' >Login</button>
+        </form>
+      </div>     
     </div>
   )
 }
