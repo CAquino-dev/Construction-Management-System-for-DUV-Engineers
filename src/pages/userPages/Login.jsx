cf3781d6182edea337200fee0174540d6f28b111
JasTenC <gorduizjustincarl@gmail.com>
2025-03-06 12:40:58 +0800

    login UI fixed

--- src/pages/userPages/Login.jsx
+++ src/pages/userPages/Login.jsx
@@ -14,9 +14,9 @@ const Login = () => {
     }
 
   return (
-    <div className='pt-16 flex items-center justify-center min-h-screen bg-[#f6f6f6]' style = {{backgroundImage: `url(${img1})`}}>
+    <div className='p-8 flex items-center justify-center min-h-screen bg-cover bg-center relative' style = {{backgroundImage: `url(${img1})`}}>
       <div className='absolute inset-0 bg-[#4c735c]/30 backdrop-blur-sm'></div>
-      <div className='bg-white rounded-2xl shadow-2lg p-8 w-full max-w-md'>
+      <div className='relative w-full max-w-md min-h-[500px] h-auto bg-white/30 backdrop-blur-lg rounded-lg shadow-xl p-10 flex flex-col justify-center'>
         <h2 className='text-2xl font-bold text-center mb-6 text-[#4c735c]'>Login</h2>
         <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
           <input 
@@ -34,6 +34,7 @@ const Login = () => {
           className='p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#4c735c]'
           />
           <button type='submit' className='bg-[#4c735c] text-white py-3 rounded-lg hover:bg-[#4c735c]/90 cursor-pointer' >Login</button>
+          <p className='text-center'>Don't have an account? <a href="/register" className='text-[#4c735c]'>Sign Up</a></p>
         </form>
       </div>     
     </div>
