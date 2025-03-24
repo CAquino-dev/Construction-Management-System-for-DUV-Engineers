de12b7183a12a57f16a07d427da102ff9fb59099
Christian.Dev <CAquino.dev@gmail.com>
2025-03-24 15:38:36 +0800

    create an api endpoint to get users per department

--- backend/routes/userManagementRoute.js
+++ backend/routes/userManagementRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUsers } = require('../controller/userManagementController')
+const { getUsers, getUsersByDepartment } = require('../controller/userManagementController')
 
 router.get('/getUsers', getUsers);
+router.get('/department/:department_id', getUsersByDepartment);
 
 module.exports = router;
