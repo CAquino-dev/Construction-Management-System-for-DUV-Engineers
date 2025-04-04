dac34a6bc444ea3d4a38279d23092279ec52bc9f
CAquino-dev <caquino.dev@gmail.com>
2025-04-04 15:00:43 +0800

    Dev kurisu (#21)
    * working userLogging in page
    
    * make the admin login route
    
    * make the adminLogin frontend working
    
    * make some changes to the database table and add an email for the register endpoint
    
    * make the userManagement controller and make the get users endpoint
    
    * edi the register so that requires a full name as well
    
    * make the user management frontend fetch the data from the database onto displaying the data in the table
    
    * create an api endpoint to get users per department
    
    * make the admin login working and store their permissions in the permissions context
    
    * Make sure all endpoints are working
    
    * fix the backend use DUV database and make permissionContext
    
    * fix the api frontend to fetch the employees from the database

--- src/pages/admin/EmployeeManagement.jsx
+++ src/pages/admin/EmployeeManagement.jsx
@@ -9,7 +9,7 @@ export const EmployeeManagement = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users/getUsers");
+        const response = await fetch("http://localhost:5000/api/users/getEmployees");
         const data = await response.json();
         setEmployees(data);
       } catch (error) {
--- src/pages/admin/HR.jsx
+++ src/pages/admin/HR.jsx
@@ -8,7 +8,7 @@ export const HR = () => {
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await fetch("http://localhost:5000/api/users/getUsers");
+                const response = await fetch("http://localhost:5000/api/users/getEmployees");
                 const data = await response.json();
                 setEmployees(data);
             } catch (error) {
