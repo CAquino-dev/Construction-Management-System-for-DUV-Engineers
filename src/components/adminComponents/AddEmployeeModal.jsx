12ee4ad2233e082081ce08e51390b79c2f921c73
JasTenC <gorduizjustincarl@gmail.com>
2025-04-11 14:12:31 +0800

    change add employee button color

--- src/components/adminComponents/AddEmployeeModal.jsx
+++ src/components/adminComponents/AddEmployeeModal.jsx
@@ -155,7 +155,7 @@ export const AddEmployeeModal = ({ isOpen, onClose, onSubmit }) => {
             <button type="button" onClick={onClose} className="px-4 py-2 bg-gray-400 text-white rounded hover:bg-gray-500 cursor-pointer">
               Cancel
             </button>
-            <button type="submit" className="bg-green-600 text-white px-4 py-2 rounded-full flex items-center hover:bg-green-700 cursor-pointer">
+            <button type="submit" className="bg-[#4c735c] text-white px-4 py-2 rounded-full flex items-center cursor-pointer">
               Add Employee
             </button>
           </div>
