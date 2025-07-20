51e809b5ed339d2fd0bd7ab6d14dd07f9436d132
JasTenC <gorduizjustincarl@gmail.com>
2025-07-20 15:05:52 +0800

    small changes in admin layout

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -4,15 +4,19 @@ import AdminNavbar from '../components/adminComponents/AdminNavbar'
 
 const AdminLayout = () => {
   return (
-    <>
-    <div className="flex min-h-screen">
-      <AdminNavbar className="" />
+    <div className="h-screen flex">
+      {/* Sidebar and Topbar are inside AdminNavbar */}
+      <AdminNavbar />
 
-      <div className="flex-1 p-6 bg-gray-100 overflow-auto">
-        <Outlet />  
+      {/* Main Content Area */}
+      <div className="flex-1 overflow-y-auto bg-gray-100 pt-16">
+        {/* pt-16 matches the height of your fixed top nav (p-4 = 64px) */}
+        {/* lg:ml-64 matches the sidebar width so content isn't hidden behind it */}
+        <div className="p-4">
+          <Outlet />
+        </div>
       </div>
     </div>
-    </>
   )
 }
 
