ca7347d4816b1594f3ccafc5d1bcc69d41cbb1f2
JasTenC <gorduizjustincarl@gmail.com>
2025-10-18 01:59:10 +0800

    component/confirmation modal/ z-index max

--- src/components/adminComponents/ConfirmationModal.jsx
+++ src/components/adminComponents/ConfirmationModal.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { useState } from "react";
 
-const ConfirmationModal = ({ isOpen, onClose, onConfirm, actionType, setRemark }) => {
+const ConfirmationModal = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  actionType,
+  setRemark,
+}) => {
   if (!isOpen) return null;
-  console.log('action', actionType)
+  console.log("action", actionType);
 
   const handleChange = (event) => {
     setRemark(event.target.value); // Update the remark text as the user types
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-900/70 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-gray-900/70 flex items-center justify-center z-10000">
       <div className="bg-white p-6 rounded-lg shadow-lg">
         <h2 className="text-lg font-semibold text-gray-800">Confirm Action</h2>
         <p className="mt-2 text-gray-600">
           Are you sure you want to <strong>{actionType}</strong>?
         </p>
 
-        {(actionType === "Rejected by HR" || actionType === "Rejected by Finance" || actionType === "Rejected") && (
+        {(actionType === "Rejected by HR" ||
+          actionType === "Rejected by Finance" ||
+          actionType === "Rejected") && (
           <div className="mt-4">
             <textarea
               className="w-full p-2 border rounded-md"
@@ -28,10 +36,16 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm, actionType, setRemark }
         )}
 
         <div className="mt-4 flex justify-end space-x-3">
-          <button onClick={onClose} className="px-4 py-2 text-gray-600 hover:text-gray-800">
+          <button
+            onClick={onClose}
+            className="px-4 py-2 text-gray-600 hover:text-gray-800"
+          >
             Cancel
           </button>
-          <button onClick={onConfirm} className="px-4 py-2 bg-[#4c735c] text-white rounded-md hover:bg-[#3a5b47]">
+          <button
+            onClick={onConfirm}
+            className="px-4 py-2 bg-[#4c735c] text-white rounded-md hover:bg-[#3a5b47]"
+          >
             Confirm
           </button>
         </div>
