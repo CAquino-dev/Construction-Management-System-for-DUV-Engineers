30d40c8a09afd70db4a179609d5d7bead13ee666
JasTenC <gorduizjustincarl@gmail.com>
2025-10-27 20:27:50 +0800

    Userlayout/ removed nav in login mobile

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import UserNavbar from '../components/userComponents/UserNavbar'
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import UserNavbar from "../components/userComponents/UserNavbar";
 
 const UserLayout = () => {
+  const location = useLocation();
+
+  // Check if current page is login
+  const isLoginPage = location.pathname === "/login";
+
   return (
     <>
-      <UserNavbar />
+      {/* Show Navbar only if NOT login — OR if login but screen is desktop */}
+      <div className={`${isLoginPage ? "hidden md:block" : ""}`}>
+        <UserNavbar />
+      </div>
+
       <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default UserLayout
+export default UserLayout;
